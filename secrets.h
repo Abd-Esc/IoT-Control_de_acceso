3e273962661aa55cb41a306e4547ee662b6fe4d0
Smith <Jhon.Smith.rv2@gmail.com>
2026-07-08 16:55:13 -0400

    URL del VPS

--- secrets.h
+++ secrets.h
@@ -6,7 +6,7 @@ static const char* WIFI_PASSWORD = "andresP23";
 
 // URL base del backend en la LAN.
 // Ejemplo: http://IP_DE_TU_PC:8000/api/v1
-static const char* API_BASE_URL = "http://192.168.0.29:8000/api/v1";
+static const char* API_BASE_URL = "http://137.184.51.90/api/v1";
 
 // Actualmente el backend no exige API key.
 static const char* DEVICE_API_KEY = "";
